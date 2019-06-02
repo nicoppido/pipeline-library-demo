e267fcee7c5f601a187ae593cf3cf1834aad2088
Nicolò  Oppido <nico.oppido@gmail.com>
2019-06-02 17:27:40 +0200

    Update info.groovy

--- vars/info.groovy
+++ vars/info.groovy
@@ -1,5 +1,9 @@
 #!/usr/bin/env groovy
 
-def info(String message = 'message') {
+//def info(String message = 'message') {
+//    echo "INFO: ${message}"
+//}
+
+def call(String message = 'message') {
     echo "INFO: ${message}"
 }
