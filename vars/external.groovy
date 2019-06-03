71dbda10b8da03a1364521eb7f31039c69f08c73
Nicolò  Oppido <nico.oppido@gmail.com>
2019-06-03 12:46:51 +0200

    Update external.groovy

--- vars/external.groovy
+++ vars/external.groovy
@@ -3,9 +3,13 @@
 @Grab('org.apache.commons:commons-math3:3.4.1')
 import org.apache.commons.math3.primes.Primes
 
-def parallelize(int count) {
-  if (!Primes.isPrime(count)) {
-    error "${count} was not prime"
-  }
-  // …
+Class external{
+
+	def parallelize(int count) {
+	  if (!Primes.isPrime(count)) {
+	    error "${count} was not prime"
+	  }
+	  // …
+	}
+	
 }
