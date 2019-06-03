7b40a0dba6e7d680e71c65082d7e219fd1c54c2d
Nicolò  Oppido <nico.oppido@gmail.com>
2019-06-03 12:25:28 +0200

    Create external.groovy

---
+++ vars/external.groovy
@@ -0,0 +1,11 @@
+#!/usr/bin/env groovy
+
+@Grab('org.apache.commons:commons-math3:3.4.1')
+import org.apache.commons.math3.primes.Primes
+
+void parallelize(int count) {
+  if (!Primes.isPrime(count)) {
+    error "${count} was not prime"
+  }
+  // …
+}
