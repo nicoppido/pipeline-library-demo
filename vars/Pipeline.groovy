7d525d401d85b8102f06b359ce087f1f420ab42f
Nicolò  Oppido <nico.oppido@gmail.com>
2019-06-11 10:30:13 +0200

    Update Pipeline.groovy

--- vars/Pipeline.groovy
+++ vars/Pipeline.groovy
@@ -2,7 +2,12 @@ def call(){
 
     try {
         pipeline {
-            agent any
+            agent {
+                docker {
+                    image 'maven:3-alpine' 
+                    args '-v /root/.m2:/root/.m2' 
+                }
+            }
             stages {
                 stage('Stage 1') {
                     steps {
