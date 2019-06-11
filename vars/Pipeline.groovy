e6f6df82b3539fe9667fe436edff69c98e1a0c9c
Nicolò  Oppido <nico.oppido@gmail.com>
2019-06-11 10:07:41 +0200

    Create Pipeline.groovy

---
+++ vars/Pipeline.groovy
@@ -0,0 +1,54 @@
+def call(){
+
+    try {
+        pipeline {
+            agent {
+                docker {
+                    image 'maven:3-alpine' 
+                    args '-v /root/.m2:/root/.m2' 
+                }
+            }
+            stages {
+                stage('Stage 1') {
+                    steps {
+                        script { 
+                            log.info 'Starting'
+                        }  
+                        sayHello 'Nicolò'
+                    }
+                }
+                stage('Stage 2') {
+                    steps {
+                        echo "Running ${env.BUILD_ID} on ${env.JENKINS_URL}"
+                        println GlobalVars.foo
+                    }
+                }
+                stage('Build') { 
+                    steps {
+                        sh 'mvn -B -DskipTests clean package' 
+                    }
+                }
+                stage('Test') {
+                    steps {
+                        sh 'mvn test'
+                    }
+                stage('Stage 3'){
+                    steps {
+                        script { 
+                            log.warning 'Nothing to do!'
+                        }   
+                    }
+                }
+                post {
+                    always {
+                       junit 'target/surefire-reports/*.xml'
+                    }
+                }
+            }
+        }
+    }
+    finally {
+        echo ""
+    }
+    
+}
