32bd0d5313b5b43b4754707a9c0faa1abed9430e
nicoppido <nico.oppido@gmail.com>
2019-06-26 12:37:01 +0200

    Update RequiredKeysException.groovy

--- src/net/test/error/RequiredKeysException.groovy
+++ src/net/test/error/RequiredKeysException.groovy
@@ -3,7 +3,7 @@ package net.test.error;
 class RequiredKeysException extends Exception {
 
     public RequiredKeysException(String key, String details){
-        super("The " + key + " is not allowed: " + details);
+        super("AAAAA The " + key + " is not allowed: " + details);
     }
 
 }
